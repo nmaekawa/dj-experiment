e68e5a69663f15c28ba4875ec1a3edfca4a61a99
nmaekawa <nmaekawa@g.harvard.edu>
2017-04-17 20:40:54 -0400

    consumer model

--- authzapp/models.py
+++ authzapp/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from datetime import timedelta
+
 from django.db import models
 from django.db.models.signals import post_save
 
@@ -6,16 +9,44 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
+    created = models.DateTimeField(default=datetime.utcnow)
+    updated = models.DateTimeField(auto_now=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    consumer_key = models.CharField(max_length=50, blank=True)
-    secret_key = models.CharField(max_length=50, blank=True)
+    prime_consumer = models.OneToOneField(
+            'Consumer',
+            related_name='prime_profile',
+            null=True,
+            on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+
+def expire_in_weeks(ttl=24):
+    return datetime.utcnow() + timedelta(weeks=ttl)
+
+
+class Consumer(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    api_key = models.CharField(max_length=50, primary_key=True)
+    secret_key = models.CharField(max_length=50, blank=False, null=False)
+    expire_on = models.DateTimeField(default=expire_in_weeks)
+    profile = models.ForeignKey(
+            'Profile',
+            related_name='consumers',
+            on_delete=models.CASCADE)
+    #prime_profile = models.OneToOneField(
+    #        Profile,
+    #        parent_link=True,
+    #        related_name='prime_consumer',
+    #        on_delete=models.CASCADE)
+
 
--- authzapp/views.py
+++ authzapp/views.py
@@ -2,5 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("hello, worl. you're at the authzapp index")
+    if request.user.is_authenticated:
+        return HttpResponse("hello, worl. you're at the authzapp index")
+    else:
+        if request.user is None:
+            return HttpResponse("hello, user is None")
+        else:
+            headers = ','.join(request.META)
+            if 'HTTP_AUTHORIZATION' in request.META:
+                msg = "header auth is {} - headers({})".format(
+                        request.META['HTTP_AUTHORIZATION'], headers)
+            else:
+                msg = "hello, user is {} - headers({})".format(
+                        request.user, headers)
+
+            return HttpResponse(msg)
 
